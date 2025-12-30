8cf206bc19be35387e0f3c0420cc67bf259ddafe
Brian Li <brianchunghli@gmail.com>
2025-12-30 11:47:39 +1100

    add draft logger layer implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use tower::Layer;
+
+#[derive(Clone, Copy)]
+pub struct HttpLogLayer;
+
+#[derive(Clone, Copy)]
+pub struct LogService<S> {
+    inner: S,
+}
+impl HttpLogLayer {
+    pub fn new() -> Self {
+        Self {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<S> Layer<S> for HttpLogLayer {
+    type Service = LogService<S>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn layer(&self, inner: S) -> Self::Service {
+        Self::Service { inner }
+    }
+}
+
+type Req = hyper::Request<hyper::body::Incoming>;
+
+impl<S> tower::Service<Req> for LogService<S>
+where
+    S: tower::Service<Req> + Clone,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, request: hyper::Request<hyper::body::Incoming>) -> Self::Future {
+        let (parts, body) = request.into_parts();
+        dbg!(&parts);
+        self.inner.call(hyper::Request::from_parts(parts, body))
     }
 }
